812fb0a2b524930ee4afac9dc1c90c5f6eb4cffe
O-01 <sa.an-s@yandex.com>
2024-05-29 15:14:59 -0500

    Modified counting array count tracking in 102-counting_sort, modified thingy variable name to 'counting' for clarity

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,7 +8,7 @@
 */
 void counting_sort(int *array, size_t size)
 {
-	int max_val = 0, *thingy = NULL, *output = NULL;
+	int max_val = 0, *counting = NULL, *output = NULL;
 	size_t iter = 0;
 
 	if (!array || size < 2)
@@ -16,20 +16,23 @@ void counting_sort(int *array, size_t size)
 	for (; iter < size; ++iter)
 		if (array[iter] > max_val)
 			max_val = array[iter];
-	thingy = calloc(max_val + 1, sizeof(int));
-	if (!thingy)
+	counting = calloc(max_val + 1, sizeof(int));
+	if (!counting)
 		return;
 	for (iter = 0; iter < size; ++iter)
-		thingy[array[iter]]++;
+		counting[array[iter]]++;
 	for (iter = 1; (int)iter < max_val + 1; ++iter)
-		thingy[iter] += thingy[iter - 1];
+		counting[iter] += counting[iter - 1];
+	print_array(counting, max_val + 1);
 	output = calloc(size, sizeof(int));
 	if (!output)
 		return;
 	for (iter = 0; iter < size; ++iter)
-		output[thingy[array[iter]] - 1] = array[iter];
+	{
+		output[counting[array[iter]] - 1] = array[iter];
+		counting[array[iter]]--;
+	}
 	for (iter = 0; iter < size; ++iter)
 		array[iter] = output[iter];
-	print_array(thingy, max_val + 1);
-	free(thingy), free(output);
+	free(counting), free(output);
 }
